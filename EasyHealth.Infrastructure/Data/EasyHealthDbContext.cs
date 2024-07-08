e95719056f2800dfb63913377dce4dfa7c9df984
Rodrigo <rodrigo.ferreiradaslv@gmail.com>
2024-07-08 03:09:16 -0300

    feat: created EasyHealthDbContext in the infrastructure layer

---
+++ EasyHealth.Infrastructure/Data/EasyHealthDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EasyHealth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyHealth.Infrastructure.Data;
+
+public class EasyHealthDbContext : DbContext
+{
+    public EasyHealthDbContext(DbContextOptions<EasyHealthDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Doctor> Doctors { get; set; } = null!;
+    public DbSet<Employee> Employees { get; set; } = null!;
+    public DbSet<Patient> Patients { get; set; } = null!;
+    public DbSet<Role> Roles { get; set; } = null!;
+    public DbSet<Specialty> Specialties { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
